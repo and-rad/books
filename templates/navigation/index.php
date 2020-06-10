89e85ffb5eae065c691246bb8fae712551e173dc
Andreas Raddau <and.rad@posteo.de>
2020-06-11 00:02:19 +0200

    Initial commit

---
+++ templates/navigation/index.php
@@ -0,0 +1,18 @@
+
+<div id='select-category'>
+	<select>
+	<option value=''>In Progress</option>
+	<option value=''>All Books</option>
+	<option value=''>Author</option>
+	<option value=''>Series</option>
+	<option value=''>Genre</option>
+	<option value=''>Status</option>
+	</select>
+</div>
+
+<ul id='list-category'>
+<li><span class='name'>Isaac Asimov</span><span class='count'>13</span></li>
+	<li class='active'><span class='name'>Arthur C. Clarke</span><span class='count'>2</span></li>
+	<li><span class='name'>Robert Heinlein</span><span class='count'>5</span></li>
+	<li><span class='name'>Dicker Vater ihm sein Sohn und seine Tochter</span><span class='count'>5</span></li>
+</ul>
