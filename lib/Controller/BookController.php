ec6b566905a09942010cd8e1f0eef7b95442214c
Andreas Raddau <and.rad@posteo.de>
2020-06-11 23:31:10 +0200

    Blocked out first API route

---
+++ lib/Controller/BookController.php
@@ -0,0 +1,21 @@
+<?php
+namespace OCA\Alexandria\Controller;
+
+use OCP\IRequest;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\JSONResponse;
+
+class BookController extends Controller {
+	public function __construct(string $AppName, IRequest $request) {
+		parent::__construct($AppName, $request);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @return JSONResponse
+	 */
+	public function index() {
+		return new JSONResponse([]);
+	}
+}
