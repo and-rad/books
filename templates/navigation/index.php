2b6ca5cf788bfb9ad0c06dbbae3674cfd4748aee
Andreas Raddau <and.rad@posteo.de>
2020-06-11 17:33:18 +0200

    Fleshed out frontend template some more

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,18 +1,18 @@
-
 <div id='select-category'>
 	<select>
-	<option value=''>In Progress</option>
-	<option value=''>All Books</option>
-	<option value=''>Author</option>
-	<option value=''>Series</option>
-	<option value=''>Genre</option>
-	<option value=''>Status</option>
+		<option value=''>All Books</option>
+		<option value=''>Authors</option>
+		<option value=''>Series</option>
+		<option value=''>Genres</option>
+		<option value=''>Read Status</option>
+		<option value=''>Shelves</option>
 	</select>
 </div>
 
 <ul id='list-category'>
-<li><span class='name'>Isaac Asimov</span><span class='count'>13</span></li>
+	<li><span class='name'>Isaac Asimov</span><span class='count'>13</span></li>
 	<li class='active'><span class='name'>Arthur C. Clarke</span><span class='count'>2</span></li>
 	<li><span class='name'>Robert Heinlein</span><span class='count'>5</span></li>
 	<li><span class='name'>Dicker Vater ihm sein Sohn und seine Tochter</span><span class='count'>5</span></li>
+	<li class='app-navigation-new'><button class='icon-add'>New Shelf</button></li>
 </ul>
--- templates/settings/index.php
+++ templates/settings/index.php
@@ -1,8 +1,12 @@
-<div id="app-settings">
-	<div id="app-settings-header">
-		<button class="settings-button" data-apps-slide-toggle="#app-settings-content">Settings</button>
+<div id='app-settings'>
+	<div id='app-settings-header'>
+		<button class='settings-button' data-apps-slide-toggle='#app-settings-content'>Settings</button>
 	</div>
-	<div id="app-settings-content">
-		<!-- Your settings in here -->
+	<div id='app-settings-content'>
+		<input type='text' id='path-settings' placeholder='Library directory'>
+		<ul>
+			<li class='app-settings-item icon-search'><button id='settings-item-scan'>Scan for books</button></li>
+			<li class='app-settings-item icon-delete'><button id='settings-item-reset'>Reset library</button></li>
+		</ul>
 	</div>
 </div>
