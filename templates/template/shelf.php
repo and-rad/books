6cafe25ac39d924db85cc45e00ba93ab36c511c3
Andreas Raddau <and.rad@posteo.de>
2020-06-17 15:45:30 +0200

    Title text overflows when list gets too narrow

--- templates/template/shelf.php
+++ templates/template/shelf.php
@@ -3,8 +3,8 @@
 		<div><span class='placeholder'></span></div>
 	</td>
 	<td class='field title'>
-		<div class='title-1'></div>
-		<div class='title-2'></div>
+		<div><span class='title-1'></span></div>
+		<div><span class='title-2'></span></div>
 	</td>
 	<td class='field author'></td>
 	<td class='field genre'></td>
