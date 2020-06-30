ab8b1ffebdc9767c45cc76dae53f793e969dfa65
Andreas Raddau <and.rad@posteo.de>
2020-06-30 09:06:48 +0200

    Created read status icons

--- templates/template/shelf.php
+++ templates/template/shelf.php
@@ -1,6 +1,11 @@
 <template id='template-shelf-item'>
 	<td class='field cover'>
-		<div><span class='placeholder'></span></div>
+		<div>
+			<span class='placeholder'></span>
+			<svg viewBox='0 0 24 24' class='icon done'><use href='/apps/books/img/icons.svg#done'/></svg>
+			<svg viewBox='0 0 24 24' class='icon pending'><use href='/apps/books/img/icons.svg#more'/></svg>
+			<svg viewBox='0 0 24 24' class='icon canceled'><use href='/apps/books/img/icons.svg#close'/></svg>
+		</div>
 	</td>
 	<td class='field title'>
 		<div><span class='title-1'></span></div>
