3b952f7ca537061ff2a699726324ac034a61510e
Andreas Raddau <and.rad@posteo.de>
2020-09-04 00:11:27 +0200

    Refactored some backend-facing JS code

--- src/app.js
+++ src/app.js
@@ -623,58 +623,58 @@ OCA.Books.UI = (function() {
  * the first parameter to that function.
  */
 OCA.Books.Backend = (function() {
+	var _get = function(uri, callback) {
+		let xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", callback);
+		xhr.open("GET", uri);
+		xhr.setRequestHeader("requesttoken", oc_requesttoken);
+		xhr.send();
+	};
+
+	var _post = function(uri, data, callback) {
+		let xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", callback);
+		xhr.open("POST", uri);
+		xhr.setRequestHeader("requesttoken", oc_requesttoken);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.send(data);
+	};
+
 	return {
-		get: function(uri, callback) {
-			let xhr = new XMLHttpRequest();
-			xhr.addEventListener("load", callback);
-			xhr.open("GET", uri);
-			xhr.setRequestHeader("requesttoken", oc_requesttoken);
-			xhr.send();
-		},
-
-		post: function(uri, data, callback) {
-			let xhr = new XMLHttpRequest();
-			xhr.addEventListener("load", callback);
-			xhr.open("POST", uri);
-			xhr.setRequestHeader("requesttoken", oc_requesttoken);
-			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-			xhr.send(data);
-		},
-
 		getConfig: function(callback) {
-			this.get(generateUrl("apps/books/api/0.1/config"), function() {
+			_get(generateUrl("apps/books/api/0.1/config"), function() {
 				callback(JSON.parse(this.response));
 			});
 		},
 
 		getBooks: function(callback) {
-			this.get(generateUrl("apps/books/api/0.1/books"), function() {
+			_get(generateUrl("apps/books/api/0.1/books"), function() {
 				callback(JSON.parse(this.response));
 			});
 		},
 
 		getLocation: function(id, callback) {
-			this.get(generateUrl("apps/books/api/0.1/loc/"+id), function() {
+			_get(generateUrl("apps/books/api/0.1/loc/"+id), function() {
 				callback(JSON.parse(this.response));
 			});
 		},
 
 		saveProgress: function(id, value, callback) {
 			let data = `id=${id}&progress=${value}`;
-			this.post(generateUrl("apps/books/api/0.1/progress"), data, function() {
+			_post(generateUrl("apps/books/api/0.1/progress"), data, function() {
 				callback(JSON.parse(this.response));
 			});
 		},
 
 		scan: function(dir, callback) {
 			let data = `dir=${dir}`;
-			this.post(generateUrl("apps/books/api/0.1/scan"), data, function() {
+			_post(generateUrl("apps/books/api/0.1/scan"), data, function() {
 				callback(JSON.parse(this.response));
 			});
 		},
 
 		reset: function(callback) {
-			this.post(generateUrl("apps/books/api/0.1/reset"), "", function() {
+			_post(generateUrl("apps/books/api/0.1/reset"), "", function() {
 				callback(JSON.parse(this.response));
 			});
 		}
